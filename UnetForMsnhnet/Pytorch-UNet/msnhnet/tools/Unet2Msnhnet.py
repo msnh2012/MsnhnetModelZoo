59c65a39b5ebf027691795117f734a28de254b69
root <wwwtyyz@163.com>
2021-09-10 00:36:04 +0800

    Fix grad

--- UnetForMsnhnet/Pytorch-UNet/msnhnet/tools/Unet2Msnhnet.py
+++ UnetForMsnhnet/Pytorch-UNet/msnhnet/tools/Unet2Msnhnet.py
@@ -8,11 +8,12 @@ sys.path.append("../..")
 
 from unet.unet_model import UNet
 
-net = UNet(3,2)
-net.load_state_dict(torch.load("weights/MODEL.pth"))
-net.to("cpu")
-net.eval()
+with torch.no_grad():
+    net = UNet(3,2)
+    net.load_state_dict(torch.load("weights/MODEL.pth"))
+    net.to("cpu")
+    net.eval()
 
-input=torch.ones([1,3,512,512])
+    input=torch.ones([1,3,512,512])
 
-trans(net, input,"unet.msnhnet","unet.msnhbin",False)
+    trans(net, input,"unet.msnhnet","unet.msnhbin")
